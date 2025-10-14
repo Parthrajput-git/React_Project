6de8a90fb65e10e284bed0de12af4248da7cc920
Parthrajput-git <pr9410350@gmail.com>
2025-10-14 22:57:56 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+// import { useState } from "react"
 import "./Dashboard.css";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
@@ -87,3 +87,4 @@ const Dashboard = ({ user }) => {
 };
 
 export default Dashboard;
+
