0d415e10700d92230fc93231eab5dafbcdf055f1
Parthrajput-git <pr9410350@gmail.com>
2025-10-16 15:41:08 +0530

    Move Login component to src/pages directory

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,5 @@
 src/pages/Login.js
-// import React, { useState } from "react";
+import React, { useState } from "react";
 // import { FaUser, FaLock } from "react-icons/fa";
 
 // function Login() {
@@ -51,4 +51,5 @@ src/pages/Login.js
 // }
 
 // export default Login;
+
 
