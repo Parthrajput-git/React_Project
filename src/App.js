4fb9c450556e43434c2c07d9ed2c277439b9e001
Parthrajput-git <pr9410350@gmail.com>
2025-10-17 10:53:37 +0530

    Uncomment import for Login page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 // import Footer from "./components/Footer";
 // import Home from "./pages/Home";
 // import Contact from "./pages/Contact";
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import About from "./pages/About.jsx";
 import Category from "./pages/Category.jsx";
