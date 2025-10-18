91f19e2ff7b6e704b8500c3cce244f952ae56cd0
Parthrajput-git <pr9410350@gmail.com>
2025-10-18 21:47:59 +0530

    Uncomment BrowserRouter import in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
