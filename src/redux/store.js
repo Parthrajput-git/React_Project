f5c678143d952f8111d6de68580fcb3a035dbe8c
Parthrajput-git <pr9410350@gmail.com>
2025-10-19 22:58:32 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from "./slices/userSlice"
 
 
-// export const store = configureStore({
+export const store = configureStore({
   // reducer: {
     // user: userSlice,
   // },
