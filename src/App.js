e13dd554c0675255b3b1161364d0830198ec1f6c
Parthrajput-git <pr9410350@gmail.com>
2025-10-29 22:43:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Profile from "./pages/Profile.jsx";
 import ResetPassword from "./pages/ResetPassword.jsx";
 import SingleLaptopInfo from "./pages/SingleLaptopInfo.jsx";
 import MyCart from "./pages/MyCart.jsx"
-// import { useState } from "react";
+import { useState } from "react";
 // import { ToastContainer } from "react-toastify";
 // import "react-toastify/dist/ReactToastify.css";
 
